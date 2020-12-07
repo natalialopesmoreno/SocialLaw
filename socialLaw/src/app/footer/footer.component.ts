1028e61e8af884363ef5d2717a90992dc56a7682
imaginaCode <imaginedreams.bfs@gmail.com>
2020-12-07 08:45:26 -0300

    footer e navBar superior

--- socialLaw/src/app/footer/footer.component.ts
+++ socialLaw/src/app/footer/footer.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { faFortAwesome } from '@fortawesome/free-brands-svg-icons';
+import{ faFacebook} from '@fortawesome/free-brands-svg-icons'
+import{ faLinkedin} from '@fortawesome/free-brands-svg-icons'
+import{ faInstagram} from '@fortawesome/free-brands-svg-icons'
+import{ faBlackTie} from '@fortawesome/free-brands-svg-icons'
+import{ faWhatsapp} from '@fortawesome/free-brands-svg-icons'
+import { faBalanceScale } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +14,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+
+  faFacebook = faFacebook
+  faLinkedin= faLinkedin
+  faInstagram = faInstagram
+  faBlackTie = faBlackTie
+  faBalanceScale = faBalanceScale
+  faWhatsapp = faWhatsapp
 
   constructor() { }
 
