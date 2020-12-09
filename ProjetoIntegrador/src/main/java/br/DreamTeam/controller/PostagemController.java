bdb2ead3549ee5665dc4cad1ba0c24fd4456de88
Natália L. Moreno <63927404+natalialopesmoreno@users.noreply.github.com>
2020-12-09 09:48:42 -0300

    alterações Backend-Angular

--- ProjetoIntegrador/src/main/java/br/DreamTeam/controller/PostagemController.java
+++ ProjetoIntegrador/src/main/java/br/DreamTeam/controller/PostagemController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import br.DreamTeam.repository.PostagemRepository;
 
 @RestController
 @RequestMapping("/postagem")
+@CrossOrigin("*")
 public class PostagemController implements WebMvcConfigurer
 {
 
